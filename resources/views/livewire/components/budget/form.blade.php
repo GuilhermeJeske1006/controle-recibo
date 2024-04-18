d3d5c4f4da5fdc61b4524ecad43c8c2eb8957fb3
guilherme <guilhermeieski@gmail.com>
2024-04-18 10:56:03 -0300

    controle-01: add loading in button when press submit

--- resources/views/livewire/components/budget/form.blade.php
+++ resources/views/livewire/components/budget/form.blade.php
@@ -145,7 +145,10 @@
         </div>
 
 
-        <button type="submit"
+        <div wire:loading wire:loading.target='submit' class="text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> 
+            Salvando...
+        </div>
+        <button type="submit" wire:loading.remove
             class="text-white mt-3 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Salvar</button>
     </form>
 
